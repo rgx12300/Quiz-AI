1b68e5e474525967c0749f1403c813e2aaab18a8
Abdulaziz Aljadaan <111672775+aljadan@users.noreply.github.com>
2024-05-11 16:24:22 +0300

    d

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.demo',
+  appName: 'Quiz AI',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className=''>
+      <iframe
+        src='https://quiz-ai-kohl.vercel.app/'
+        frameBorder='0'
+        className='w-full h-screen'
+      ></iframe>
+    </div>
+  )
+}
+
+export default App
